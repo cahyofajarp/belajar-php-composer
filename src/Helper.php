4a77fe30b7394ee3331c67dc17fa5aa6d814cf38
cahyo fajar pamungkas <cahyofajar28@gmail.com>
2021-07-14 18:40:38 +0700

    Tambahan Helper

--- src/Helper.php
+++ src/Helper.php
@@ -16,6 +16,7 @@ class Helper{
     // }
     
     private string $name;
+    private string $result;
 
     public function SayHello(string $name):string
     {
@@ -23,7 +24,19 @@ class Helper{
 
         return 'Hello, ' .$this->name;
     }
+
+    public function checkGenap(?int $no): string
+    {
+        if($no % 2 == 0){
+            $this->result = 'Genap';
+
+            return $this->result;
+        }
+    }
+
 }
+
+
 
 // $data = new Helper;
 
